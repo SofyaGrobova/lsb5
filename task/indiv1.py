e4601c3461857aa791160eba32c9c9b8cc48046e
SofyaGrobova <90389589+SofyaGrobova@users.noreply.github.com>
2022-01-18 14:24:43 +0300

    Update indiv1.py

--- task/indiv1.py
+++ task/indiv1.py
@@ -3,30 +3,30 @@
 
 if __name__ == '__main__':
 
-m = int(input("ВВедите номер месяца"))
-if m == 1:
-    print("Январь")
-elif m == 2:
-    print("Февраль")
-elif m == 3:
-    print("Март")
-elif m == 4:
-    print("Апрель")
-elif m == 5:
-    print("Май")
-elif m == 6:
-    print("Июнь")
-elif m == 7:
-    print("Июль")
-elif m == 8:
-    print("Август")
-elif m == 9:
-    print("Сентябрь")
-elif m == 10:
-    print("Октябрь")
-elif m == 11:
-    print("Ноябрь")
-elif m == 12:
-    print("Декабрь")
-else:
-    print("Не коректный номер месяца")
+    m = int(input("ВВедите номер месяца"))
+    if m == 1:
+        print("Январь")
+    elif m == 2:
+        print("Февраль")
+    elif m == 3:
+        print("Март")
+    elif m == 4:
+        print("Апрель")
+    elif m == 5:
+        print("Май")
+    elif m == 6:
+        print("Июнь")
+    elif m == 7:
+        print("Июль")
+    elif m == 8:
+        print("Август")
+    elif m == 9:
+        print("Сентябрь")
+    elif m == 10:
+        print("Октябрь")
+    elif m == 11:
+        print("Ноябрь")
+    elif m == 12:
+        print("Декабрь")
+    else:
+        print("Не коректный номер месяца")
