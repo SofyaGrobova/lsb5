8efed6f82bc6d60836f21a6ebeab9f2ec5c38c21
SofyaGrobova <90389589+SofyaGrobova@users.noreply.github.com>
2022-01-18 14:21:50 +0300

    Update indiv2.py

--- task/indiv2.py
+++ task/indiv2.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import math
 
+if __name__ == '__main__':
 a = int(input("Введите a:"))
 b = int(input("Введите b:"))
 c = int(input("Введите c:"))
